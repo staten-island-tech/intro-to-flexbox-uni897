0ed27b87df39dd8c4483bececfbd2b9246b44fa7
uni897 <akhasmeer7@gmail.com>
2025-09-25 13:52:47 -0400

    js

--- app.js
+++ app.js
@@ -42,4 +42,36 @@ const name = "Daddy Owen";
 console.log(`I love ${name}`);
 
 console.log(5 % 2);
+
+const age = 18;
+if (age >= 18) {
+    console.log("Vote");
+}else{
+    console.log("No vote");
+}
+
+const temp = 32;
+if (temp >= 212) {
+    console.log("Boiling");
+}else if (temp <= 32) {
+    console.log("Freezing");
+}else{
+    console.log("water");
+}
+const age = 35;
+const citizen = true;
+
+if (age === true || citizen === true){
+    console.log("You can run for president");
+}
 */
+
+function enterCastle(key, magicpowers, isBanned){
+    if ((key || magicpowers) && !isBanned){
+        console.log("You may enter the castle");
+    }else{
+        console.log("You may not enter the castle");
+    }
+}
+
+enterCastle(true, true, true);
